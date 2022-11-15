e82089f08c9bf246b4c4f01ab878da8b997b1f78
OlegRyaboschuk <olegr12032002@gmail.com>
2022-11-15 12:34:32 +0200

    second commit

--- js/script.js
+++ js/script.js
@@ -37,36 +37,37 @@ $('#cityB').on('click', function(){
 $('#cityC').text('в' + ' ' + city);	 
 function dataHandler3(data) {
 	dataString = JSON.stringify(data);
+	console.log(data);
 	var now = new Date();
 	let h = now.getHours();
-    var num = 8-(Math.floor(h/3));
+    var num = 1;
 
-		document.getElementById("demo6").innerHTML = data.list[num+3].dt_txt;
-		document.getElementById("demo4").innerHTML = "Макс." + " " + Math.floor((data.list[num+3].main["temp"])-273,15)+"°C";
-		document.getElementById("demo7").innerHTML = data.list[num].dt_txt;
-		document.getElementById("demo5").innerHTML = "Мін." +  " " + Math.floor((data.list[num].main["temp"])-273,15)+"°C";
-		var imgURL = "https://openweathermap.org/img/w/" + data.list[num+3].weather[0].icon + ".png";
+		// document.getElementById("demo6").innerHTML = data.list[num].dt_txt;
+		document.getElementById("demo4").innerHTML = "Макс." + " " + Math.floor((data.list[num].main["temp_max"])-273,15)+"°C";
+		// document.getElementById("demo7").innerHTML = data.list[num].dt_txt;
+		document.getElementById("demo5").innerHTML = "Мін." +  " " + Math.floor((data.list[num].main["temp_min"])-273,15)+"°C";
+		var imgURL = "https://openweathermap.org/img/w/" + data.list[num].weather[0].icon + ".png";
 		$("#tmp4").attr("src", imgURL);
 
-    document.getElementById("demo8").innerHTML = data.list[num+11].dt_txt;
-		document.getElementById("demo9").innerHTML = "Макс. " + " " + Math.floor((data.list[num+11].main["temp"])-273,15)+"°C";
-		document.getElementById("demo10").innerHTML = data.list[num+8].dt_txt;
-		document.getElementById("demo11").innerHTML = "Мин." +  " " + Math.floor((data.list[num+9].main["temp"])-273,15)+"°C";
-		var imgURL = "https://openweathermap.org/img/w/" + data.list[num+11].weather[0].icon + ".png";
+    // document.getElementById("demo8").innerHTML = data.list[num+1].dt_txt;
+		document.getElementById("demo9").innerHTML = "Макс. " + " " + Math.floor((data.list[num+1].main["temp_max"])-273,15)+"°C";
+		// document.getElementById("demo10").innerHTML = data.list[num+1].dt_txt;
+		document.getElementById("demo11").innerHTML = "Мин." +  " " + Math.floor((data.list[num+1].main["temp_min"])-273,15)+"°C";
+		var imgURL = "https://openweathermap.org/img/w/" + data.list[num+1].weather[0].icon + ".png";
 		$("#tmp5").attr("src", imgURL);
 
-    document.getElementById("demo12").innerHTML = data.list[num+19].dt_txt;
-		document.getElementById("demo13").innerHTML = "Макс." + " " + Math.floor((data.list[num+19].main["temp"])-273,15)+"°C";
-		document.getElementById("demo14").innerHTML = data.list[num+16].dt_txt;
-		document.getElementById("demo15").innerHTML = "Мин." +  " " + Math.floor((data.list[num+17].main["temp"])-273,15)+"°C";
-        var imgURL = "https://openweathermap.org/img/w/" + data.list[num+19].weather[0].icon + ".png";
+    // document.getElementById("demo12").innerHTML = data.list[num+2].dt_txt;
+		document.getElementById("demo13").innerHTML = "Макс." + " " + Math.floor((data.list[num+2].main["temp_max"])-273,15)+"°C";
+		// document.getElementById("demo14").innerHTML = data.list[num+2].dt_txt;
+		document.getElementById("demo15").innerHTML = "Мин." +  " " + Math.floor((data.list[num+2].main["temp_min"])-273,15)+"°C";
+        var imgURL = "https://openweathermap.org/img/w/" + data.list[num+2].weather[0].icon + ".png";
 		$("#tmp6").attr("src", imgURL);		
 
-    document.getElementById("demo16").innerHTML = data.list[num+27].dt_txt;
-		document.getElementById("demo17").innerHTML = "Макс." + " " + Math.floor((data.list[num+27].main["temp"])-273,15)+"°C";
-		document.getElementById("demo18").innerHTML = data.list[num+24].dt_txt;
-		document.getElementById("demo19").innerHTML = "Мин." +  " " + Math.floor((data.list[num+25].main["temp"])-273,15)+"°C";
-        var imgURL = "https://openweathermap.org/img/w/" + data.list[num+27].weather[0].icon + ".png";
+    // document.getElementById("demo16").innerHTML = data.list[num+3].dt_txt;
+		document.getElementById("demo17").innerHTML = "Макс." + " " + Math.floor((data.list[num+3].main["temp_max"])-273,15)+"°C";
+		// document.getElementById("demo18").innerHTML = data.list[num+3].dt_txt;
+		document.getElementById("demo19").innerHTML = "Мин." +  " " + Math.floor((data.list[num+3].main["temp_min"])-273,15)+"°C";
+        var imgURL = "https://openweathermap.org/img/w/" + data.list[num+3].weather[0].icon + ".png";
 		$("#tmp7").attr("src", imgURL);		
 				
    }
@@ -89,30 +90,30 @@ function dataHandler3(data) {
 					weekday[6] = "Субота";
 					 
 					 if(d.getDay() >= 3){
-						   n1 = weekday[(d.getDay()+1)];
-						   n2 = weekday[(d.getDay()+2)];
-						   n3 = weekday[(d.getDay()+3)];
-						   n4 = weekday[7-(d.getDay()+4)];} if(d.getDay() >= 4)
+						   n1 = weekday[(d.getDay())];
+						   n2 = weekday[(d.getDay()+1)];
+						   n3 = weekday[(d.getDay()+2)];
+						   n4 = weekday[7-(d.getDay()+3)];} if(d.getDay() >= 4)
 					     {
-						   n1 = weekday[(d.getDay()+1)];
-						   n2 = weekday[(d.getDay()+2)];
-						   n3 = weekday[7-(d.getDay()+3)];
-						   n4 = weekday[9-(d.getDay()+4)];} if(d.getDay() >= 5)
+						   n1 = weekday[(d.getDay())];
+						   n2 = weekday[(d.getDay()+1)];
+						   n3 = weekday[7-(d.getDay()+2)];
+						   n4 = weekday[9-(d.getDay()+3)];} if(d.getDay() >= 5)
 					     {
-						   n1 = weekday[(d.getDay()+1)];
-						   n2 = weekday[7-(d.getDay()+2)];
-						   n3 = weekday[9-(d.getDay()+3)];
-						   n4 = weekday[11-(d.getDay()+4)];} if(d.getDay() >= 6)
+						   n1 = weekday[(d.getDay())];
+						   n2 = weekday[7-(d.getDay()+1)];
+						   n3 = weekday[9-(d.getDay()+2)];
+						   n4 = weekday[11-(d.getDay()+3)];} if(d.getDay() >= 6)
 					     {
-						   n1 = weekday[7-(d.getDay()+1)];
-						   n2 = weekday[9-(d.getDay()+2)];
-						   n3 = weekday[11-(d.getDay()+3)];
-						   n4 = weekday[13-(d.getDay()+4)];}  if(d.getDay() < 3) 
+						   n1 = weekday[7-(d.getDay())];
+						   n2 = weekday[9-(d.getDay()+1)];
+						   n3 = weekday[11-(d.getDay()+2)];
+						   n4 = weekday[13-(d.getDay()+3)];}  if(d.getDay() < 3) 
 						 {
-					       n1 = weekday[(d.getDay()+1)];
-						   n2 = weekday[(d.getDay()+2)];
-						   n3 = weekday[(d.getDay()+3)];
-						   n4 = weekday[(d.getDay()+4)];
+					       n1 = weekday[(d.getDay())];
+						   n2 = weekday[(d.getDay()+1)];
+						   n3 = weekday[(d.getDay()+2)];
+						   n4 = weekday[(d.getDay()+3)];
 						  
 					     }
 						
